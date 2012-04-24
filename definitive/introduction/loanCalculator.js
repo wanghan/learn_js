31ceb3e0859faa29f996ffe795657e7a3783a5a6
Han Wang <hanwang.work@gmail.com>
2012-04-25 01:54:36 +0800

    practise in introduction
    add html mockup
    add main logic of calculate function
    add window.onload function
    add save function to save form into local storage.

---
+++ definitive/introduction/loanCalculator.js
@@ -0,0 +1,56 @@
+function calculate() {
+    // look up the input and output element
+    var amount = document.getElementById("amount");
+    var inter = document.getElementById("interest");
+    var months = document.getElementById("repayment");
+    var zipcode = document.getElementById("zipcode");
+    var payment = document.getElementById("payment");
+    var total = document.getElementById("total");
+    var totalInterest = document.getElementById("totalinterest");
+
+    // convert input to number value
+    var principle = parseFloat(amount.value);
+    var interests = parseFloat(inter.value) / 100 / 12;
+    var payments = parseFloat(months.value) / 12;
+
+    //compute monthly payment figure
+    var x = Math.pow(1 + interests, payments);
+    var monthly = (principle * x * interests) / (x - 1)
+
+    //If the result is finite number, the input was good and we have meaningful results to display
+    if (isFinite(monthly)) {
+        payment.innerHTML = monthly.toFixed(2);
+        total.innerHTML = (monthly * payments).toFixed(2)
+        totalinterest.innerHTML = ((monthly * payments) - principle).toFixed(2);
+
+        //TODO: Save the user's input
+        save(amount.value, inter.value, months.value, zipcode.value);
+    } else {
+        payment.innerHTML = "";
+        total.innerHTML = "";
+        totalinterest.innerHTML = "";
+    }
+}
+
+
+// Save the user's input as properties of the localStorage object
+
+function save(amount, intes, years, zipcode) {
+    if (window.localStorage) {
+        localStorage.loan_amount = amount;
+        localStorage.loan_interest = intes;
+        localStorage.loan_years = years;
+        localStorage.loan_zipcode = zipcode;
+    }
+}
+
+// Restore local storage when document first loads.
+window.onload = function() {
+    if (window.localStorage && window.localStorage.loan_amount) {
+        document.getElementById("amount").value = window.localStorage.loan_amount;
+        document.getElementById("interest").value = window.localStorage.loan_interest;
+        document.getElementById("repayment").value = window.localStorage.loan_years;
+        document.getElementById("zipcode").value = window.localStorage.loan_zipcode;
+        calculate();
+    }
+}
