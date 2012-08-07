100ebc38ce21ff0130447611c9e0686a09c3471d
Han Wang <hanwang.work@gmail.com>
2012-08-07 16:11:08 +0800

    Auto-fill ECP logon form

---
+++ tools/autoFillLogon.js
@@ -0,0 +1,19 @@
+﻿//Auto-fill ECP logon form
+
+function autofill() {
+
+    var usernameInput = document.getElementById("username");
+    var passwordInput = document.getElementById("password");
+    usernameInput.value = "administrator";
+
+    if (location.hostname.trim().indexOf("bj") >= 0) {
+        passwordInput.value = "J$p1ter";
+    }
+    else {
+        passwordInput.value = "T%nt0wn";
+    }
+
+    var submitInput = document.getElementsByName("logonForm");
+    submitInput[0].submit();
+}
+autofill();
