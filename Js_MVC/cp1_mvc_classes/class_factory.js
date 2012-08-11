c2236d85b6369dfab014efdb2339b797217ca1fc
Han Wang <hanwang.work@gmail.com>
2012-08-11 20:15:59 +0800

    add inheritance to class library.

--- Js_MVC/cp1_mvc_classes/class_factory.js
+++ Js_MVC/cp1_mvc_classes/class_factory.js
@@ -2,22 +2,34 @@
     class library includes functionality for instantiating and initializing instances.    
 */
 
-var Class = function () {
+var Class = function (parent) {
     var _class = function () {
         this.init.apply(this, arguments);
     }
 
+    // change _class's prototype
+    if (parent) {
+        var subclass = function () { };
+        subclass.prototype = parent.prototype;
+        _class.prototype = new subclass;
+    }
+
     _class.prototype.init = function () { };
 
+
+
+    //#region Aliases
     //shortcut to access prototype;
     _class.fn = _class.prototype;
 
     //shortcut to access class
     _class.fn.parent = _class;
+    _class._super = _class.__proto__;
+    //#endregion
 
     //#region Methods
 
-    //adding class static properties
+    //adding class properties
     _class.extend = function (obj) {
         //callback function
         var extended = obj.extended;
@@ -27,7 +39,7 @@ var Class = function () {
         if (extended) extended(_class);
     };
 
-    //adding instance properties
+    //adding instance properties to prototype
     _class.include = function (obj) {
         //callback function
         var included = obj.included;
